ff64efefae2514550be6b116a2ce30ec0563617b
Gabriel Dickman <gabrieldickman33@gmail.com>
2022-07-27 21:36:42 -0400

    Criado CRUD

--- src/app.js
+++ src/app.js
@@ -1,7 +1,71 @@
 const express = require('express');
+const { PrismaClient } = require('@prisma/client');
+const { json } = require('express');
 
 const app = express();
+const prisma = new PrismaClient();
 
-/*Para acessar o sistema localmente, abra o terminal, navegue até a pasta src e execute o comando node app e depois digite no navegador: "localhost:3333"*/
-app.listen(3333, () => console.log('Servidor rodando...'));
+app.use(express.json());
 
+app.get('/avisos', async (req, res) =>{
+
+    const avisos = await prisma.aviso.findMany() 
+
+    return res.json(avisos)
+} );
+
+app.post('/avisos', async (req, res) =>{
+
+    const aviso = await prisma.aviso.create({
+        data: {
+            content: req.body.content,
+            date: req.body.date
+        }
+    }) 
+    
+    res.status(201).json({ message: 'Aviso criado com sucesso'})
+})
+
+app.delete('/avisos/:id', async (req, res) =>{
+
+    const aviso = await prisma.aviso.delete({
+        where : {
+            id : parseInt(req.params.id)
+        }
+    })
+
+    res.status(200).json({ message: 'Aviso deletado com sucesso'})
+
+})
+
+app.put('/avisos/:id', async (req, res) =>{
+    const aviso = await prisma.aviso.update({
+        where : {
+            id : parseInt(req.params.id)
+        },
+
+        data : {
+            content : req.body.content
+        }
+    })
+
+    res.status(200).json({ message : 'Aviso atualizado com sucesso'})
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(80, ()=> console.log("Servidor rodando..."));
