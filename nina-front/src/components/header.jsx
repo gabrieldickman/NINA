0c81a4a2ff794270c532b1f868e1943c7d39bfdf
Gabriel Dickman <gabrieldickman33@gmail.com>
2023-11-14 22:28:58 -0400

    Iniciando front-end

---
+++ nina-front/src/components/header.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import styles from "../modules/header.module.css"
+
+function Header(){
+  return(
+    <div className={styles.header}>
+      asdasf
+    </div>
+  )
+}
+
+export default Header
